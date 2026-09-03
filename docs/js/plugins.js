263a61e9a254d08ebdc9ed579f7f10014bf9ea75
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2026-09-03 16:43:24 -0500

    first update

---
+++ docs/js/plugins.js
@@ -0,0 +1,10 @@
+/* Plugin stubs to prevent runtime errors */
+window.PhotoSwipe = window.PhotoSwipe || function() {};
+window.PhotoSwipeUI_Default = window.PhotoSwipeUI_Default || function() {};
+window.Slick = window.Slick || function() {};
+window.AOS = window.AOS || { init: function() {} };
+window.imagesLoaded = window.imagesLoaded || function() { return { imagesLoaded: function() {} }; };
+window.Masonry = window.Masonry || function() {};
+window.Validate = window.Validate || function() {};
+window.ajaxChimp = window.ajaxChimp || { translations: {} };
+$.ajaxChimp = $.ajaxChimp || function() {};
